1ee48ec938b31ab1cdc38b5fa77534958f2edfb6
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-03-12 15:19:55 +0100

    feat: initial commit

---
+++ backend/src/config.rs
@@ -0,0 +1,60 @@
+use centaurus::{
+  config::{BaseConfig, MetricsConfig},
+  db::config::DBConfig,
+};
+use figment::{
+  Figment,
+  providers::{Env, Serialized},
+};
+use serde::{Deserialize, Serialize};
+use tracing::instrument;
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct Config {
+  #[serde(flatten)]
+  pub base: BaseConfig,
+  #[serde(flatten)]
+  pub db: DBConfig,
+  #[serde(flatten)]
+  pub metrics: MetricsConfig,
+
+  pub db_url: String,
+
+  pub auth_pepper: String,
+  pub auth_issuer: String,
+  pub auth_jwt_expiration: i64,
+}
+
+impl Default for Config {
+  fn default() -> Self {
+    Self {
+      base: BaseConfig::default(),
+      db: DBConfig::default(),
+      db_url: "".to_string(),
+      metrics: MetricsConfig {
+        metrics_name: "hibernation".to_string(),
+        ..Default::default()
+      },
+      auth_pepper: "__HIBERNATION_PEPPER__".to_string(),
+      auth_issuer: "hibernation_auth".to_string(),
+      auth_jwt_expiration: 60 * 60 * 24 * 7, // 7 days
+    }
+  }
+}
+
+impl Config {
+  #[instrument]
+  pub fn parse() -> Self {
+    let config = Figment::new()
+      .merge(Serialized::defaults(Self::default()))
+      .merge(Env::raw().global());
+
+    let config: Self = config.extract().expect("Failed to parse configuration");
+
+    if config.db_url.is_empty() {
+      panic!("Database URL is not set");
+    }
+
+    config
+  }
+}
---
+++ frontend/src/lib/backend/updater.svelte.ts
@@ -0,0 +1,76 @@
+import { browser } from '$app/environment';
+import { invalidate } from '$app/navigation';
+import { sleep } from 'positron-components/util/interval.svelte';
+
+export enum UpdateType {
+  Settings = 'Settings',
+  Users = 'Users',
+  Groups = 'Groups'
+}
+
+export type UpdateMessage = {
+  type: UpdateType.Settings | UpdateType.Users | UpdateType.Groups;
+};
+
+let updater: WebSocket | undefined | false = $state(browser && undefined);
+let interval: number;
+let disconnect = false;
+
+export const connectWebsocket = () => {
+  if (updater === false || updater) return;
+  createWebsocket();
+};
+
+const createWebsocket = () => {
+  updater = new WebSocket('/api/ws/updater');
+
+  updater.onmessage = (event) => {
+    const msg: UpdateMessage = JSON.parse(event.data);
+    handleMessage(msg);
+  };
+
+  updater.onclose = async () => {
+    clearInterval(interval);
+    if (disconnect) return;
+    await sleep(1000);
+    createWebsocket();
+  };
+
+  interval = setInterval(() => {
+    if (
+      !updater ||
+      updater.readyState === updater.CLOSING ||
+      updater.readyState === updater.CLOSED
+    ) {
+      clearInterval(interval);
+      return;
+    }
+
+    updater.send('heartbeat');
+  }, 10000) as unknown as number;
+};
+
+export const disconnectWebsocket = () => {
+  if (updater) {
+    disconnect = true;
+    updater.close();
+    updater = undefined;
+  }
+};
+
+const handleMessage = (msg: UpdateMessage) => {
+  switch (msg.type) {
+    case UpdateType.Settings: {
+      invalidate((url) => url.pathname.startsWith('/api/settings'));
+      break;
+    }
+    case UpdateType.Users: {
+      invalidate((url) => url.pathname.startsWith('/api/user'));
+      break;
+    }
+    case UpdateType.Groups: {
+      invalidate((url) => url.pathname.startsWith('/api/group'));
+      break;
+    }
+  }
+};
