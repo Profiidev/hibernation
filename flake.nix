ba207d130793495ec8f4a6b46cc0b0b8068d60e6
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-05-27 22:14:56 +0200

    chore: use only one nixpkgs for flake

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
     nix-filter.url = "github:numtide/nix-filter";
   };
 
