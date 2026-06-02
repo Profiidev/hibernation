b6e0c3bf02281150ac85cc83f5ad76beb8812608
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-06-02 16:20:55 +0200

    chore: updated inputs

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
