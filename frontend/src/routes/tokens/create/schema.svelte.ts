c6c003141881b62d5a44e326696b60d26c7e601c
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-08-01 07:00:10 +0200

    chore: fix lint warning

--- frontend/src/routes/tokens/create/schema.svelte.ts
+++ frontend/src/routes/tokens/create/schema.svelte.ts
@@ -3,6 +3,7 @@ import z from 'zod';
 export const information = z.object({
   exp: z
     .date('Invalid date')
+    // oxlint-disable-next-line max-nested-calls
     .default(new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000)),
   name: z.string().min(1, 'Name is required').default('') // Default to 30 days
 });
